bc3b3d57e4e82e55ddaf5120f49e1487cfb2c9cc
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-01 23:04:29 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './componentes/Header';
 
 
 function App() {
   return (
     <div className="App">
        <h1>Estudando React com Cesar Celke</h1>
+       <Header />
     </div>
   );
 }
