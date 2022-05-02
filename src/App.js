40d39ec4f12ce3d06fc61fc372760a06e0d28dd7
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-01 23:32:51 -0300

    commit carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import './App.css';
-import Header from './componentes/Header';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Carousel from "react-bootstrap/Carousel";
 
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-       <h1>Estudando React com Cesar Celke</h1>
-       <Header />
+    <div style={{ display: "block", width: 700, padding: 30 }}>
+      <h4>React-Bootstrap Carousel Component</h4>
+      <Carousel>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+            alt="One"
+          />
+          <Carousel.Caption>
+            <h3>Label for first slide</h3>
+            <p>Sample Text for Image One</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100"
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+            alt="Two"
+          />
+          <Carousel.Caption>
+            <h3>Label for second slide</h3>
+            <p>Sample Text for Image Two</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
-
-export default App;
